2c0f6b6aae534cc1e6972511a37fc9cbbf589f46
ritesh90256 <riteshminchinal365@gmail.com>
2026-06-05 11:24:24 +0530

    url changes in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from sqlalchemy import create_engine, text
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/Vigil"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 app = FastAPI()
